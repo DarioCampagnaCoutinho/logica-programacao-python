93c385b398751fa49f863a03d5ea2778fb92e80d
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-22 18:54:48 -0300

    Aulas, um exemplo com cores em python.

---
+++ modulo05-cores/aulas/aula01.py
@@ -0,0 +1,6 @@
+print('\033[4;30;43mCódigo de Cores\033[m')
+
+a = 4
+b = 5
+print('Os valores sáo \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))
+print('Os valores sáo \033[4;30;43m{}\033[m e \033[31m{}\033[m'.format(a, b))
