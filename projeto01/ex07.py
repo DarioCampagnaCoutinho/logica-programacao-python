77e08eb04853a38e9959b5a1796d32f8172d36f8
Dario Campagna Coutinho <campagnacoutinho67@gmail.com>
2021-01-06 18:57:51 -0300

    Criando um jogo com python

---
+++ projeto01/ex07.py
@@ -0,0 +1,46 @@
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
+
+print('Escolha um nível!')
+print('{1 - Facil} - {2 - Médio} - {3 - Dificil}')
+nivel = int(input('Defina o nível'))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou {} pontos.".format(pontos))
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos  - pontos_perdidos
+
+print("Fim do jogo")
