d4812bc281be3443b43307a37243c411abc78299
Dario Campagna <campagnacoutinho67@gmail.com>
2021-01-24 10:34:17 -0300

    Jogo Forca 04

--- projeto02/forca.py
+++ projeto02/forca.py
@@ -1,13 +1,22 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = []
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
 
-    for letra in palavra_secreta:
-        letras_acertadas.append('_')
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
