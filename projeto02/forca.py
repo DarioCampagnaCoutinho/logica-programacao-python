b494bb6ef226c89f4bcfc66f964987046aba692d
Dario Campagna <campagnacoutinho67@gmail.com>
2021-01-26 19:02:49 -0300

    Jogo Forca 06

--- projeto02/forca.py
+++ projeto02/forca.py
@@ -84,7 +84,6 @@ def desenha_forca(erros):
     print()
 
 
-
 def imprime_mensagem_vencedor():
     print("Parabéns, você ganhou!")
     print("       ___________      ")
@@ -119,6 +118,7 @@ def imprime_mensagem_perdedor(palavra_secreta):
     print("     \_         _/         ")
     print("       \_______/           ")
 
+
 def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
     index = 0
     for letra in palavra_secreta:
@@ -126,18 +126,22 @@ def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
             letras_acertadas[index] = letra
         index += 1
 
+
 def pede_chute():
     chute = input("Qual letra? ")
     chute = chute.strip().upper()
     return chute
 
+
 def inicializa_letras_acertadas(palavra):
     return ["_" for letra in palavra]
+
 
 def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
 
 def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
